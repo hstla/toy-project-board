c325723aedc51671c5c3766d85e69f6951c2a4bc
seong <ree7243@gmail.com>
2023-06-19 00:50:26 +0900

    feat: member entity 추가

---
+++ src/main/java/toyproject/board/domain/member/Member.java
@@ -0,0 +1,39 @@
+package toyproject.board.domain.member;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    private Long id;
+    private String email;
+    private String password;
+    private String writer;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    @Builder
+    public Member(Long id, String email, String password, String writer, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.writer = writer;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+
+    public void updateModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public void updateWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+}
