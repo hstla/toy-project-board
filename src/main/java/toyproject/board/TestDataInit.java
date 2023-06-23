9a9b0cc840e0af46b9523a851d254317ecdb05f9
seong <ree7243@gmail.com>
2023-06-24 00:57:57 +0900

    feat: member test data 추가

--- src/main/java/toyproject/board/TestDataInit.java
+++ src/main/java/toyproject/board/TestDataInit.java
@@ -2,6 +2,9 @@ package toyproject.board;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import toyproject.board.domain.member.MapMemberRepository;
+import toyproject.board.domain.member.Member;
+import toyproject.board.domain.member.MemberRepository;
 import toyproject.board.domain.post.MapPostRepository;
 import toyproject.board.domain.post.Post;
 
@@ -11,11 +14,18 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 public class TestDataInit {
     private final MapPostRepository mapPostRepository;
+    private final MapMemberRepository mapMemberRepository;
 
     @PostConstruct
     public void init() {
         mapPostRepository.save(new Post("writer1", "title1","content1"));
         mapPostRepository.save(new Post("writer2", "title2","content2"));
         mapPostRepository.save(new Post("writer3", "title3","content3"));
+        Member member = Member.builder()
+                .email("test@gmail.com")
+                .password("1234")
+                .writer("testWriter")
+                .build();
+        mapMemberRepository.save(member);
     }
 }
