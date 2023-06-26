46d7f79213e6edecfbec94b73d47287543947699
seong <ree7243@gmail.com>
2023-06-26 20:28:25 +0900

    style: @RequestMapping 애노테이션으로 코드 분리

--- src/main/java/toyproject/board/web/member/MemberController.java
+++ src/main/java/toyproject/board/web/member/MemberController.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import toyproject.board.domain.member.MapMemberRepository;
 import toyproject.board.domain.member.Member;
 import toyproject.board.domain.member.MemberService;
@@ -20,32 +17,33 @@ import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
     private final MapMemberRepository mapMemberRepository;
 
-    @GetMapping("/member/{id}")
+    @GetMapping("/{id}")
     public String user(@PathVariable Long id, Model model) {
         Member findMember = mapMemberRepository.findById(id);
         model.addAttribute("form", findMember);
         return "member/member";
     }
 
-    @GetMapping("/member/edit/{id}")
+    @GetMapping("/edit/{id}")
     public String updateMemberForm(@PathVariable Long id, Model model) {
         Member findMember = mapMemberRepository.findById(id);
         model.addAttribute("form", findMember);
         return "member/editForm";
     }
 
-    @PostMapping("/member/edit/{id}")
+    @PostMapping("/edit/{id}")
     public String updateMember(@PathVariable Long id, @Validated @ModelAttribute("form") MemberUpdateForm memberUpdateForm, BindingResult bindingResult) {
         mapMemberRepository.updateMember(id, memberUpdateForm.FormTOMember());
         return "redirect:/member/{id}";
     }
 
-    @GetMapping("/member/withdrawal/{id}")
+    @GetMapping("/withdrawal/{id}")
     public String memberWithdrawal(@PathVariable Long id, HttpServletRequest request) {
         memberService.withdrawal(id);
 
